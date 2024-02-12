dd090e8822e24a00a4a6d186c04be55ff5d64e9c
jameel <jameel@DESKTOP-2910DVO>
2024-02-13 01:14:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Python.Runtime;
+
+// ...
+
+static void GetAirlines()
+{
+   // Runtime.PythonDLL = @"C:\Users\jameel\AppData\Local\Programs\Python\Python310\Python310.dll";
+    PythonEngine.Initialize();
+    using (Py.GIL())
+    {
+        dynamic flightradar24 = Py.Import("flightradar24");
+
+        dynamic j = flightradar24.FlightRadar24API(null);
+        dynamic m = j.get_airlines();
+
+        Console.WriteLine(m);
+    }
+}
+
+// ...
+
+GetAirlines();
---
+++ lib/PythonApplication1.py
@@ -0,0 +1,8 @@
+
+import os
+def say_hello():
+    return "hallo world script python"
+def test(massage):
+ directory = os.getcwd()
+ return massage +":" +directory
+print(say_hello())
